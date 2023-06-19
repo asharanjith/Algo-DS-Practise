418b62db92cb0518f7b5dee89ef1412ce4ffc177
asharanjith <ashasvijayan@gmail.com>
2023-06-19 17:17:22 +0400

    Array permutation

---
+++ Array/Array-Permutation/main.js
@@ -0,0 +1,11 @@
+const buildArray = function(nums) { 
+    let ans = [];
+    for(let i = 0; i < nums.length; i++) {
+        ans.push(nums[nums[i]]);
+    }
+    return ans;  
+};
+
+console.log(buildArray([0,2,1,5,3,4])); // [0,1,2,4,5,3]
+console.log(buildArray([5,0,1,2,3,4])); // [4,5,0,1,2,3]
+
